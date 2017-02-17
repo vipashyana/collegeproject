7014c90349091cb1cd6ef22c06bffee73d8d4829
vipashyana <cchicky654@gmail.com>
2017-02-17 09:59:27 +0530

    Update configure.php

--- includes/configure.php
+++ includes/configure.php
@@ -1,12 +1,13 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'mytutr');
-   $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-   if (mysqli_connect_errno())
+   /* Establishing connection with mysql database*/
+   define('DB_SERVER', 'localhost'); /* defines server path*/
+   define('DB_USERNAME', 'root'); /* defines server username*/
+   define('DB_PASSWORD', '');  /* defines server password*/
+   define('DB_DATABASE', 'mytutr'); /* defines server database name*/
+   $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE); 
+   if (mysqli_connect_errno()) /* establishes connection*/
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-  mysqli_select_db($conn,"mytutr");
+  mysqli_select_db($conn,"mytutr"); /* selects databse for query processing*/
 ?>
