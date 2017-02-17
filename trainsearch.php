db497e37358604fab78f048051cf8c33d140c193
vipashyana <cchicky654@gmail.com>
2017-02-17 10:15:48 +0530

    Update trainsearch.php

--- trainsearch.php
+++ trainsearch.php
@@ -1,14 +1,16 @@
 <?php
-include ('includes/configure.php');
+include ('includes/configure.php');  /*inclides configure file for database connection*/
 
+/* $_GET['term'] stores the value inserted in train search form in live-status.php*/
 if (isset($_GET['term'])) {
   $query=$_GET['term'];
-  $s = ("SELECT tno, tname FROM train WHERE tno LIKE '%{$query}%' OR tname LIKE '%{$query}%'");
+  $s = ("SELECT tno, tname FROM train WHERE tno LIKE '%{$query}%' OR tname LIKE '%{$query}%'"); /*query to select all possible match with trai number or name inserted by user*/
   $sql=$conn->query($s);
 $array = array();
-  while ($row = $sql->fetch_assoc()) {
+  /*fetches the array in $row from $sql query execution and prints one by one in a row*/
+  while ($row = $sql->fetch_assoc()) {   
       $array[] = array (
-          'label' => $row['tno'].'-'.$row['tname'],
+          'label' => $row['tno'].'-'.$row['tname'], 
           'value' => $row['tno'],
       );
   }
